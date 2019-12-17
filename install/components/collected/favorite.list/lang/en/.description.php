5658991d993b17b167a3373be54be163a7a6b0c2
ASDAFF <asdaff.asad@yandex.ru>
2019-12-18 02:48:48 +0500

    start

---
+++ install/components/collected/favorite.list/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS['RS_FAVORITE_COM_NAME'] = 'Favorite list';
+$MESS['RS_FAVORITE_DESCRIPTION'] = 'Component to output identifiers watchlist';
+$MESS['COLLECT_COM_COMPONENTS'] = 'ASDAFF';
+$MESS['COLLECT_FAVORITE_COM_SEC_NAME'] = 'Favorite';
---
+++ install/components/collected/favorite.list/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS['RS_FAVORITE_COM_NAME'] = 'Список избранного';
+$MESS['RS_FAVORITE_DESCRIPTION'] = 'Компонент для вывода идентификаторов избранного';
+$MESS['COLLECT_COM_COMPONENTS'] = 'ASDAFF';
+$MESS['COLLECT_FAVORITE_COM_SEC_NAME'] = 'Избранное';
